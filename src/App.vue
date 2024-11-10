50568af311e6ede7ec23ccdf53e56311a343d7b1
tomoda-kazuya <tomoda1207.work@gmail.com>
2024-11-10 23:39:04 +0900

    feat: 🎸 firstcommit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { PBtn } from "@t10a/petra-ui";
+</script>
+
+<template>
+  <!-- UIコンポーネントライブラリのテスト -->
+  <p-btn label="test" :primary="true"></p-btn>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+
+// NOTE: 　Styleを読み込まないといけない
+import '@t10a/petra-ui/dist/style.css';
+
+createApp(App).mount('#app')
